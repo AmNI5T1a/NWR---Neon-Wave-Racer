9e1bdbfc1fab05a21123fd8440d2e18118cdc9ce
Tima <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2021-07-30 05:23:18 -0700

    Notification window

--- Assets/Scripts/Special Scripts/Lobby/UI/UI_Animations/ShowHideAnimations.cs
+++ Assets/Scripts/Special Scripts/Lobby/UI/UI_Animations/ShowHideAnimations.cs
@@ -14,43 +14,49 @@ namespace NWR.Lobby
 
         private Vector2 leap;
 
-        public ShowHideAnimations(GameObject instance, Vector2 position_ON_Screen, Vector2 position_OUT_ofScreen, Vector2 leap)
+        public ShowHideAnimations(GameObject gameObjectToAnimate, Vector2 position_ON_Screen, Vector2 position_OUT_ofScreen, Vector2 leap)
         {
-            this.objectToAnimate = instance;
+            this.objectToAnimate = gameObjectToAnimate;
             this.position_ON_Screen = position_ON_Screen;
             this.position_OUT_ofScreen = position_OUT_ofScreen;
             this.leap = leap;
         }
 
-        private Sequence _sequence;
+        public Sequence sequence;
         private static void KillSequence(Sequence instance)
         {
             instance.Kill();
         }
         public IEnumerator ShowAnimation()
         {
-            _sequence = DOTween.Sequence();
-            this.objectToAnimate.GetComponent<CanvasGroup>().interactable = true;
+            sequence = DOTween.Sequence();
+
+            // ? Should i move logic turn on/off interactability to root script
+            if (this.objectToAnimate.GetComponent<CanvasGroup>() != null)
+                this.objectToAnimate.GetComponent<CanvasGroup>().interactable = true;
             RectTransform rectTransform = this.objectToAnimate.GetComponent<RectTransform>();
 
-            _sequence.Append(rectTransform.DOAnchorPos(position_ON_Screen - leap, 0.35f));
-            _sequence.Append(rectTransform.DOAnchorPos(position_ON_Screen, 0.15f));
+            sequence.Append(rectTransform.DOAnchorPos(position_ON_Screen - leap, 0.35f));
+            sequence.Append(rectTransform.DOAnchorPos(position_ON_Screen, 0.15f));
             yield return new WaitForSeconds(1.05f);
 
-            _sequence.AppendCallback(() => { KillSequence(_sequence); });
+            sequence.AppendCallback(() => { KillSequence(sequence); });
         }
 
         public IEnumerator HideAnimation()
         {
-            _sequence = DOTween.Sequence();
-            this.objectToAnimate.GetComponent<CanvasGroup>().interactable = false;
+            sequence = DOTween.Sequence();
+
+            // ? Should i move logic turn on/off interactability to root script
+            if (this.objectToAnimate.GetComponent<CanvasGroup>() != null)
+                this.objectToAnimate.GetComponent<CanvasGroup>().interactable = false;
             RectTransform rectTransform = this.objectToAnimate.GetComponent<RectTransform>();
 
-            _sequence.Append(rectTransform.DOAnchorPos(position_ON_Screen - leap, 0.15f));
-            _sequence.Append(rectTransform.DOAnchorPos(position_OUT_ofScreen, 0.35f));
+            sequence.Append(rectTransform.DOAnchorPos(position_ON_Screen - leap, 0.15f));
+            sequence.Append(rectTransform.DOAnchorPos(position_OUT_ofScreen, 0.35f));
             yield return new WaitForSeconds(1.05f);
 
-            _sequence.AppendCallback(() => { KillSequence(_sequence); });
+            sequence.AppendCallback(() => { KillSequence(sequence); });
         }
     }
 }
--- Assets/Scripts/Special Scripts/Lobby/UI/UI_NotEnoughtMoney.cs
+++ Assets/Scripts/Special Scripts/Lobby/UI/UI_NotEnoughtMoney.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using NWR.Modules;
 using UnityEngine;
 
+
 namespace NWR.Lobby
 {
-    public class UI_NotEnoughtMoney : Singleton<UI_NotEnoughtMoney>
+    public class UI_NotEnoughtMoney : MonoBehaviour
     {
+        [Header("Stats:")]
+        [SerializeField] private float timeOnScreen = 0.7f;
+
+        [SerializeField] private Vector2 position_ON_Screen;
+        [SerializeField] private Vector2 position_OUT_Screen;
+
+        [SerializeField] private Vector2 leap;
+
         [Header("Info: ")]
         [ReadOnly, SerializeField] private GameObject notificationWindow;
-        void Awake()
-        {
-            notificationWindow = Instantiate(Resources.Load<GameObject>("NotificationWindow (NotEnoughMoney)"), GameObject.FindGameObjectWithTag("Canvas").transform);
-            this.gameObject.transform.parent = notificationWindow.transform;
-        }
 
-        public void ShowNotificationWindow(float showTime)
+        public void ShowNotificationWindow(ref GameObject instance)
         {
-            StartCoroutine(PopUpNotificationWindow(showTime));
+            this.notificationWindow = instance;
+            StartCoroutine(PopUpNotificationWindow());
         }
-        private IEnumerator PopUpNotificationWindow(float showTime)
+        private IEnumerator PopUpNotificationWindow()
         {
-            yield return new WaitForSeconds(showTime);
+            ShowHideAnimations animations = new ShowHideAnimations(gameObjectToAnimate: notificationWindow,
+                                                                    position_ON_Screen: position_ON_Screen,
+                                                                    position_OUT_ofScreen: position_OUT_Screen,
+                                                                    leap: leap);
+
+            StartCoroutine(animations.ShowAnimation());
+            yield return new WaitForSeconds(timeOnScreen);
+            StartCoroutine(animations.HideAnimation());
+            yield return new WaitUntil(() => !animations.sequence.active);
             Destroy(notificationWindow);
         }
     }
